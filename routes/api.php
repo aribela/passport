beee605e59277bc2ab4bd5c8049ad4a4ae532f29
aribela <52586224+aribela@users.noreply.github.com>
2022-08-15 11:03:55 -0500

    Ejemplo passport login
    Ejemplo de laravel passport login (ver imagenes de prueba)

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\PassportController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('register', [PassportController::class, 'register']);
+Route::post('login', [PassportController::class, 'login']);
+
+// put all api protected routes here
+Route::middleware('auth:api')->group(function () {
+    Route::post('user-detail', [PassportController::class, 'userDetail']);
+    Route::post('logout', [PassportController::class, 'logout']);
+});
